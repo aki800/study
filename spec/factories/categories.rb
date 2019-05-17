a03ec459721f669cc134ac184bcc06b23d3ee44c
kahorno <yamadakaho@yamadakahonoMacBook-Pro.local>
2019-05-17 23:08:30 +0900

    Tweet destroy edit

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :category do
-    
+    name {Faker::Team.name}
+    ancestry {"1"}
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     nickname              {"abe"}
     sequence(:email) {Faker::Internet.email}
     password              {"00000000"}
-    password_confirmation {"00000000"}
+    sequence(:email) {Faker::Internet.email}
   end
 
 end

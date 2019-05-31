78161aa97948ac1a5baea92bc0199ba1dd4109ca
kahorno <yamadakaho@yamadakahonoMacBook-Pro.local>
2019-05-31 22:55:55 +0900

    user edit function

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   root 'categories#index'
   resources :users
   resources :categories do

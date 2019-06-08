09e533556866f86b91d7819853e902e891c9b3b1
kahorno <yamadakaho@yamadakahonoMacBook-Pro.local>
2019-06-08 23:05:57 +0900

    Modify view

--- app/assets/javascripts/category.js
+++ app/assets/javascripts/category.js
@@ -2,30 +2,30 @@ $(document).on('turbolinks:load', function() {
   $(function() {
 
     function showMiddleCategories(category) {
-      var html = `<div class = "categories-wrapper__box", id = "middlecategory_${category.name}">
-                    <div class= "categories-wrapper__box__title">
-                      <div class= "categories-wrapper__box__title__text">
+      var html = `<div class = "categories__box", id = "middlecategory_${category.name}">
+                    <div class= "categories__box__title">
+                      <div class= "categories__box__title__text">
                         ${category.name}
                       </div>
                     </div>
                   </div>`
-      $(".categories-wrapper").append(html);
+      $(".categories").append(html);
     }
 
     function showFinalCategories(category) {
-      var html = `<a href = "/categories/${category.id}/tweets", class = "categories-wrapper__box", id = "middlecategory_${category.name}">
-                    <div class = "categories-wrapper__box__title">
-                      <div class = "categories-wrapper__box__title__text">
+      var html = `<a href = "/categories/${category.id}/tweets", class = "categories__box", id = "middlecategory_${category.name}">
+                    <div class = "categories__box__title">
+                      <div class = "categories__box__title__text">
                         ${category.name}
                       </div>
                     </div>
                   </a>`
-      $(".categories-wrapper").append(html);
+      $(".categories").append(html);
     }
 
 
     $("#category_勉強").on("click", function() {
-      $(".categories-wrapper").empty();
+      $(".categories").empty();
         for (var n = 0, sub_len = gon.study_categories.length; n < sub_len - 1; ++n) {
         showMiddleCategories(gon.study_categories[n]);
         }
@@ -34,31 +34,31 @@ $(document).on('turbolinks:load', function() {
         }
 
       $("#middlecategory_受験勉強").on("click", function() {
-        $(".categories-wrapper").empty();
+        $(".categories").empty();
         for (var i = 0, sub_sub_len = gon.examination_categories.length; i < sub_sub_len; ++i) {
         showFinalCategories(gon.examination_categories[i]);
         }
       });
       $("#middlecategory_資格勉強").on("click", function() {
-        $(".categories-wrapper").empty();
+        $(".categories").empty();
         for (var i = 0, sub_sub_len = gon.certification_categories.length; i < sub_sub_len; ++i) {
         showFinalCategories(gon.certification_categories[i]);
         }
       });
       $("#middlecategory_語学勉強").on("click", function() {
-        $(".categories-wrapper").empty();
+        $(".categories").empty();
         for (var i = 0, sub_sub_len = gon.language_categories.length; i < sub_sub_len; ++i) {
         showFinalCategories(gon.language_categories[i]);
         }
       });
       $("#middlecategory_学問").on("click", function() {
-        $(".categories-wrapper").empty();
+        $(".categories").empty();
         for (var i = 0, sub_sub_len = gon.learning_categories.length; i < sub_sub_len; ++i) {
         showFinalCategories(gon.learning_categories[i]);
         }
       });
       $("#middlecategory_ビジネス").on("click", function() {
-        $(".categories-wrapper").empty();
+        $(".categories").empty();
         for (var i = 0, sub_sub_len = gon.business_categories.length; i < sub_sub_len; ++i) {
         showFinalCategories(gon.business_categories[i]);
         }
@@ -67,7 +67,7 @@ $(document).on('turbolinks:load', function() {
 
 
     $("#category_運動").on("click", function() {
-      $(".categories-wrapper").empty();
+      $(".categories").empty();
       for (var n = 0, sub_len = gon.exercise_categories.length; n < sub_len; ++n) {
         if(gon.exercise_categories[n].name == "スポーツ") {
           showMiddleCategories(gon.exercise_categories[n]);
@@ -78,7 +78,7 @@ $(document).on('turbolinks:load', function() {
       }
 
         $("#middlecategory_スポーツ").on("click", function() {
-          $(".categories-wrapper").empty();
+          $(".categories").empty();
           for (var i = 0, sub_sub_len = gon.sport_categories.length; i < sub_sub_len; ++i) {
           showFinalCategories(gon.sport_categories[i]);
           }
@@ -87,7 +87,7 @@ $(document).on('turbolinks:load', function() {
 
 
     $("#category_健康").on("click", function() {
-      $(".categories-wrapper").empty();
+      $(".categories").empty();
         for (var n = 0, sub_len = gon.health_categories.length; n < sub_len; ++n) {
         showFinalCategories(gon.health_categories[n]);
         }
@@ -95,7 +95,7 @@ $(document).on('turbolinks:load', function() {
 
 
     $("#category_趣味・創作活動").on("click", function() {
-      $(".categories-wrapper").empty();
+      $(".categories").empty();
         for (var n = 0, sub_len = gon.hobby_categories.length; n < sub_len; ++n) {
           if(gon.hobby_categories[n].name == "その他の趣味・創作活動") {
           showFinalCategories(gon.hobby_categories[n]);
@@ -106,25 +106,25 @@ $(document).on('turbolinks:load', function() {
         }
 
         $("#middlecategory_読書").on("click", function() {
-          $(".categories-wrapper").empty();
+          $(".categories").empty();
           for (var i = 0, sub_sub_len = gon.book_categories.length; i < sub_sub_len; ++i) {
             showFinalCategories(gon.book_categories[i]);
           }
         });
         $("#middlecategory_楽器演奏").on("click", function() {
-          $(".categories-wrapper").empty();
+          $(".categories").empty();
           for (var i = 0, sub_sub_len = gon.music_instrument_categories.length; i < sub_sub_len; ++i) {
             showFinalCategories(gon.music_instrument_categories[i]);
           }
         });
         $("#middlecategory_絵を描く").on("click", function() {
-          $(".categories-wrapper").empty();
+          $(".categories").empty();
           for (var i = 0, sub_sub_len = gon.painting_categories.length; i < sub_sub_len; ++i) {
             showFinalCategories(gon.painting_categories[i]);
           }
         });
         $("#middlecategory_執筆").on("click", function() {
-          $(".categories-wrapper").empty();
+          $(".categories").empty();
           for (var i = 0, sub_sub_len = gon.writing_categories.length; i < sub_sub_len; ++i) {
             showFinalCategories(gon.writing_categories[i]);
           }
